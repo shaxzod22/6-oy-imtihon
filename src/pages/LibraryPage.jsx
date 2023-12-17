ee32f5ae8da10136fd0a7504e488c2ab313bc8c6
shaxzod22 <shaxzod01901@gmail.com>
2023-12-17 09:49:19 +0500

    some fixes

--- src/pages/LibraryPage.jsx
+++ src/pages/LibraryPage.jsx
@@ -1,10 +1,9 @@
-import { useContext } from "react"
-import {InputContext} from '../App'
+
 const LibraryPage = () => {
-  const {search} = useContext(InputContext)
+  
   return (
     <div>
-      It is Library Page{search}
+      It is Library Page
     </div>
   )
 }
